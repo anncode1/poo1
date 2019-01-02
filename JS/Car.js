8c83d805f97c708e131c587483ccb55e1f0b86d8
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-01-02 06:38:13 -0500

    Metodo impresiion datos en JS

--- JS/Car.js
+++ JS/Car.js
@@ -7,3 +7,10 @@ function Car(license, driver) {
     this.passenger;
     
 }
+
+Car.prototype.printDataCar = function () {
+    console.log(this.driver);
+    console.log(this.driver.name);
+    console.log(this.driver.document);
+    console.log(this.passengers);
+}
--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,7 @@
 
 var car = new Car("AW4567", new Account("Andres", "AND123"));
-car.passengers = 4
-console.log(car.driver);
-console.log(car.driver.name);
-console.log(car.driver.document);
-console.log(car.passengers);
+car.passengers = 4;
+car.printDataCar();
 
 
 
